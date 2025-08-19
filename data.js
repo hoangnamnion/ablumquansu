9e371974d83afcdbc6a6cafa0d633024f6cb5766
Cao Văn Nam <caovannamutt@gmail.com>
2025-08-19 20:09:26 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,17 @@
+
+window.APP_DATA = [
+  {
+    type: 'image',
+    src: 'https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?w=1600',
+    title: 'Hoàng hôn',
+    description: 'Khoảnh khắc hoàng hôn trên núi.',
+    alt: 'Sunset over mountains'
+  },
+  {
+    type: 'video',
+    src: 'https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4',
+    poster: 'https://images.unsplash.com/photo-1441974231531-c6227db76b6e?w=1200',
+    title: 'Timelapse hoa nở',
+    description: 'Một đoạn timelapse ngắn.'
+  },
+];
